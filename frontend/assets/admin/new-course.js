1e435cd056b55e111bf79fa5779a137a91301e1a
Mattz <matias.marti@medieinstitutet.com>
2026-03-05 11:21:42 +0100

    form submision for new course

--- frontend/assets/admin/new-course.js
+++ frontend/assets/admin/new-course.js
@@ -14,3 +14,25 @@ export function createCourse(course) {
         yield client.from('courses').insert(course).select();
     });
 }
+document.getElementById('new-course-form').addEventListener('submit', (e) => __awaiter(void 0, void 0, void 0, function* () {
+    var _a, _b, _c, _d;
+    e.preventDefault();
+    debugger;
+    const formData = new FormData(e.target);
+    const course = {
+        id: crypto.randomUUID(),
+        created_at: new Date().toISOString(),
+        average_rating: 0,
+        description: '',
+        image_url: '',
+        rating_count: 0,
+        type: '', //ts workaround
+        start_date: new Date(new Date().setMonth(new Date().getMonth() + 1)).toISOString(), // +1 month from now
+        title: (_a = formData.get('title')) === null || _a === void 0 ? void 0 : _a.trim(),
+        course_number: (_b = formData.get('number')) === null || _b === void 0 ? void 0 : _b.trim(),
+        duration_days: Number((_c = formData.get('days')) === null || _c === void 0 ? void 0 : _c.trim()),
+        price: Number((_d = formData.get('cost')) === null || _d === void 0 ? void 0 : _d.trim()),
+    };
+    yield createCourse(course);
+    window.history.back();
+}));
--- frontend/assets/admin/new-course.ts
+++ frontend/assets/admin/new-course.ts
@@ -5,3 +5,31 @@ import { client, Course } from "../db.js"
 export async function createCourse(course: Course) {
     await client.from('courses').insert(course).select()
 }
+
+
+document.getElementById('new-course-form')!.addEventListener('submit', async (e: SubmitEvent) => {
+    e.preventDefault()
+    debugger
+
+    const formData = new FormData(e.target as HTMLFormElement)
+    const course = {
+      id: crypto.randomUUID(),
+      created_at: new Date().toISOString(),
+      average_rating: 0,
+      description: '',
+      image_url: '',
+      rating_count: 0,
+      type: '' as '', //ts workaround
+      start_date: new Date(new Date().setMonth(new Date().getMonth() + 1)).toISOString(), // +1 month from now
+
+
+      title: (formData.get('title') as string)?.trim(),
+      course_number: (formData.get('number') as string)?.trim(),
+      duration_days: Number((formData.get('days') as string)?.trim()),
+      price: Number((formData.get('cost') as string)?.trim()),
+    }
+
+    await createCourse(course)
+
+    window.history.back()
+})
